8a33946634d1a7e30ddfadd3c0761d8658343526
RaspberryPi <bek97t@mail.ru>
2020-01-20 23:20:18 +0300

    add Send and foo

--- receivedemo.cpp
+++ receivedemo.cpp
@@ -1,33 +1,23 @@
 #include <stdio.h>
+#include <fstream>
 #include "rc-switch/RCSwitch.h"
 #include <time.h>
+#include <iostream>
+#include <string>
 
-int main(int argc, char *argv[]) {
 
-    printf("Starting receivedemo\n");
-    printf("\n");
-    uint32_t Data = 0;
+using namespace std;
+
+ uint32_t Data = 0;
     int T1,T2,T3;
     int PIN = 2; // this is pin 13, aka GPIO22 on the PI3, see https://www.element14.com/community/servlet/JiveServlet/previewBody/73950-102-10-339300/pi3_gpio.png
 
-    if (wiringPiSetup () == -1) {
-        printf("ERROR\n");
-        return 1;
-    }
-
-    // put the PIN into no-pull/up down state:
-    // see https://github.com/ninjablocks/433Utils/issues/21
-    pullUpDnControl(PIN, PUD_OFF);
-
     RCSwitch mySwitch = RCSwitch();
 
-    mySwitch.enableReceive(PIN);
 
-    printf("Listening\n");
+
+void foo(){
     time_t tOld;
-    int flag=0;
-    while(true) {
-
         if (mySwitch.available())
         {
 		time_t t = time(NULL);
@@ -47,18 +37,40 @@ int main(int argc, char *argv[]) {
          }
         }
         //________________________________________Чтение и отправка
-        FILE* Send = fopen("/var/www/html/Termo/server/Send","r");
-        if( Send != NULL && flag != 1 )
+	ifstream f1("/var/www/html/Termo/server/Send");
+	if (f1.is_open())
         {
-            int64_t command;
-            fscanf(Send, "%i", &command);
-            printf("Sended : %i", command);
-            fclose(Send);
-            flag = 1;
-            //remove("/var/www/html/Termo/server/Send");
+	   uint32_t command;
+	    f1 >> command;
+	   cout << command << endl;
+    	// Optional set number of transmission repetitions.
+   	 // mySwitch.setRepeatTransmit(15);
+	    mySwitch.send(command, 32);
+       	    f1.close();
+            remove("/var/www/html/Termo/server/Send");
         }
-        else
-            printf("Cannot open directory file.");
         delay(100);
+}
+
+
+int main(int argc, char *argv[]) {
+
+    printf("Starting receivedemo\n");
+    printf("\n");
+   
+    if (wiringPiSetup () == -1) {
+        printf("ERROR\n");
+        return 1;
+    }
+    pullUpDnControl(PIN, PUD_OFF);
+    // put the PIN into no-pull/up down state:
+    // see https://github.com/ninjablocks/433Utils/issues/21
+    mySwitch.enableReceive(PIN);
+    mySwitch.enableTransmit(0);
+    mySwitch.setProtocol(1);
+
+    printf("Listening\n");
+    while(true) {
+	foo();
     }
 }
